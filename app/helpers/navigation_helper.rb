01eb728cb084426e4729673a8f949b7e5e86b687
David Van Der Beek <earlynovrock@gmail.com>
2015-04-04 15:49:56 -0500

    handle various link types

--- app/helpers/navigation_helper.rb
+++ app/helpers/navigation_helper.rb
@@ -6,6 +6,6 @@ module NavigationHelper
   end
 
   def prototype_partial_link(text, partial, target, options = {})
-    link_to text, prototype_page_path(page: partial), remote: true, class: "prototype-partial-link #{options[:class]}", data: { target: target }
+    link_to text, prototype_page_path(page: partial), remote: true, class: "prototype-partial-link #{options[:class]}", data: { target: target }, style: options[:style]
   end
 end
